91d66740858ee9305305e025aed83d5a5fd0e043
aimee_t <aimeetemple14@gmail.com>
2024-04-29 14:29:16 -0400

    Initial commit

---
+++ Animations2/Animations2App.swift
@@ -0,0 +1,17 @@
+//
+//  Animations2App.swift
+//  Animations2
+//
+//  Created by Temple on 2024-04-29.
+//
+
+import SwiftUI
+
+@main
+struct Animations2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Animations2/ContentView.swift
@@ -0,0 +1,37 @@
+//
+//  ContentView.swift
+//  Animations2
+//
+//  Created by Temple on 2024-04-29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    
+    @State private var dragAmount = CGSize.zero
+    
+    var body: some View {
+        
+        LinearGradient(colors: [.yellow, .red], startPoint: .topLeading, endPoint: .bottomTrailing)
+            .frame(width: 300, height: 200)
+            .clipShape(.rect(cornerRadius: 10))
+            .offset(dragAmount)
+            .gesture(
+            DragGesture()
+                .onChanged { dragAmount = $0.translation }
+                .onEnded { _ in
+                    withAnimation(.bouncy) {
+                    dragAmount = .zero
+                    }
+                }
+            )
+        // Implicit bouncy animation
+        // .animation(.bouncy, value: dragAmount)
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
